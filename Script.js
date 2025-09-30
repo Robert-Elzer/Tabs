ea6f9c42b8fd66ced3bafeb731a954f1c1ef1843
Robert-Elzer <soldier1807@gmail.com>
2025-09-30 13:43:19 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,19 @@
+function openPage(pageName, elmnt, color) {
+    let i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = 'none';
+    }
+    
+    tablinks = document.getElementsByClassName("tabLink");
+    for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(pageName).style.display = "block";
+
+    elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
